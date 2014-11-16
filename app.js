fb131076f77c287e1f3c6050d91927bfd06df36d
Varun Arora <a.varun1997@gmail.com>
2014-11-16 04:32:09 -0500

    Authentication added

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 var express = require("express");
+var session = require('express-session');
 var app = express();
 var http = require("http").createServer(app);
 var bodyParser = require("body-parser");
 var io = require("socket.io").listen(http);
+var passport = require('passport');
+var WindowsLiveStrategy = require('passport-windowslive').Strategy;
 var _ = require("underscore");
+
+var WINDOWS_LIVE_CLIENT_ID = "0000000048131F76"
+var WINDOWS_LIVE_CLIENT_SECRET = "IXTlXb4o1xLuxRxL3mwbhmAofBf5XRa8";
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Windows Live profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new WindowsLiveStrategy({
+    clientID: WINDOWS_LIVE_CLIENT_ID,
+    clientSecret: WINDOWS_LIVE_CLIENT_SECRET,
+    callbackURL: "http://4fe37ae7.ngrok.com/auth/windowslive/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Windows Live profile is returned
+      // to represent the logged-in user.  In a typical application, you would
+      // want to associate the Windows Live account with a user record in your
+      // database, and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 /* 
   The list of participants in our chatroom.
@@ -25,34 +64,73 @@ app.use(express.static("public", __dirname + "/public"));
 
 app.use(bodyParser.json());
 
+app.use(session({secret: 'fat people'}))
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
 //route homepage
-app.get("/", function(request, response) {
-  response.render("index");
+app.get("/", function(req, res) {
+  res.render("index", { user: req.user });
+});
+
+// GET /auth/windowslive
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Windows Live authentication will involve
+//   redirecting the user to live.com.  After authorization, Windows Live
+//   will redirect the user back to this application at
+//   /auth/windowslive/callback
+app.get('/auth/windowslive',
+  passport.authenticate('windowslive', { scope: ['wl.signin', 'wl.basic'] }),
+  function(req, res){
+    // The request will be redirected to Windows Live for authentication, so
+    // this function will not be called.
+  });
+
+// GET /auth/windowslive/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the hub page.
+app.get('/auth/windowslive/callback', 
+  passport.authenticate('windowslive', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/hub');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 //route boards
-app.get("/board", function(request, response) {
-  response.render("board");
+app.get("/board", function(req, res) {
+  res.render("board");
 });
 
-app.post("/message", function(request, response) {
+app.get("/hub", function(req, res) {
+  res.render("hub");
+});
+
+app.post("/message", function(req, res) {
 
   //The request body expects a param named "message"
-  var message = request.body.message;
+  var message = req.body.message;
 
   //If the message is empty or wasn't sent it's a bad request
   if(_.isUndefined(message) || _.isEmpty(message.trim())) {
-    return response.json(400, {error: "Message is invalid"});
+    return res.json(400, {error: "Message is invalid"});
   }
 
   //We also expect the sender's name with the message
-  var name = request.body.name;
+  var name = req.body.name;
 
   //Let our chatroom know there was a new message
   io.sockets.emit("incomingMessage", {message: message, name: name});
 
   //Looks good, let the client know
-  response.json(200, {message: "Message received"});
+  res.json(200, {message: "Message received"});
 
 });
 
